db7c8c7c836688891a420f32df49e03d0e3f7971
pierre818181 <pierre.bastola@runpod.io>
2024-11-30 18:56:57 -0800

    runpod intrinsic changes

--- src/token.ts
+++ src/token.ts
@@ -7,9 +7,9 @@ import {
   Authenticator,
 } from "./auth";
 
-export function importKeyFromBase64(key: string): JsonWebKey {
+export function importKeyFromBase64(key: string): string {
   // Decodes the base64 value and performs unicode normalization.
-  return JSON.parse(decode(key));
+  return decode(key);
 }
 
 export async function newRegistryTokens(jwtPublicKey: string): Promise<RegistryTokens> {
@@ -17,10 +17,10 @@ export async function newRegistryTokens(jwtPublicKey: string): Promise<RegistryT
 }
 
 export class RegistryTokens implements Authenticator {
-  private jwtPublicKey: JsonWebKey;
+  private jwtPublicKey: string;
   authmode: string;
 
-  constructor(jwtPublicKey: JsonWebKey) {
+  constructor(jwtPublicKey: string) {
     this.authmode = "RegistryTokens";
     this.jwtPublicKey = jwtPublicKey;
   }
@@ -68,13 +68,13 @@ export class RegistryTokens implements Authenticator {
     };
 
     const token = await jwt.sign(tokenPayload, privateKey, {
-      algorithm: "ES256",
+      algorithm: "RS256",
     });
 
     return token;
   }
 
-  static checkIfV2OnlyPath(request: Request): boolean {
+  checkIfV2OnlyPath(request: Request): boolean {
     return request.url.endsWith("/v2/");
   }
 
@@ -87,7 +87,7 @@ export class RegistryTokens implements Authenticator {
   }> {
     try {
       // first verify the JWT
-      if (!(await jwt.verify(token, this.jwtPublicKey, { algorithm: "ES256" }))) {
+      if (!(await jwt.verify(token, this.jwtPublicKey, { algorithm: "RS256" }))) {
         console.warn("verifyToken: jwt.verify() failed");
         return { verified: false, payload: null };
       }
@@ -95,7 +95,7 @@ export class RegistryTokens implements Authenticator {
       // the JWT signature is valid, decode it now
       const decoded = jwt.decode(token);
       const payload = decoded.payload as RegistryAuthProtocolTokenPayload;
-      return RegistryTokens.verifyPayload(request, payload);
+      return this.verifyPayload(request, payload);
     } catch (error) {
       // If the verification fails (e.g., due to token expiration or signature mismatch),
       // jwt.verify() will throw an error which we can catch here.
@@ -107,7 +107,7 @@ export class RegistryTokens implements Authenticator {
     }
   }
 
-  static verifyPayload(request: Request, payload: RegistryAuthProtocolTokenPayload) {
+  verifyPayload(request: Request, payload: RegistryAuthProtocolTokenPayload) {
     // Check if token has expired
     const now = Math.floor(Date.now() / 1000);
     if (payload.exp && now >= payload.exp) {
@@ -115,28 +115,37 @@ export class RegistryTokens implements Authenticator {
       console.warn(`verifyV0Token: failed jwt verification: the token has expired`);
       return { verified: false, payload: null };
     }
-
     // ensure capabilities are satisfied
     switch (request.method) {
       // PULL or PUSH methods
       case "HEAD":
         // HEAD requests can be used by pushers like docker
-        if (!payload.capabilities.includes("pull") && !payload.capabilities.includes("push")) {
+        if (!payload.capabilities.includes("pull") || !payload.capabilities.includes("push")) {
           console.warn(
             `verifyToken: failed jwt verification: missing any capability for HEAD request in ${request.url}`,
+          );
+          return { verified: false, payload: null };
+        }
+        if (!checkHasPermissionToImage(payload, request)) {
+          console.warn(
+            `verifyToken: failed jwt verification: image name ${payload?.imageName} does not match the token's image name in ${request.url}`,
           );
           return { verified: false, payload: null };
         }
         break;
       // PULL method
       case "GET":
+        if (this.checkIfV2OnlyPath(request)) {
+          return { verified: true, payload };
+        }
+
+        if (request.url == 'https://registry.runpod.net/' || request.url == 'https://registry.runpod.net') {
+          return { verified: true, payload }
+        }
+
         if (this.checkIfV2OnlyPath(request) && payload.capabilities.length === 0) {
           console.warn("verifyToken: failed jwt verification: missing any capabilities for GET request in /v2/");
           return { verified: false, payload: null };
-        }
-
-        if (this.checkIfV2OnlyPath(request)) {
-          return { verified: true, payload };
         }
 
         if (!payload.capabilities.includes("pull")) {
@@ -145,10 +154,28 @@ export class RegistryTokens implements Authenticator {
           );
           return { verified: false, payload: null };
         }
+        if (!checkHasPermissionToImage(payload, request)) {
+          console.warn(
+            `verifyToken: failed jwt verification: image name ${payload?.imageName} does not match the token's image name in ${request.url}`,
+          );
+          return { verified: false, payload: null };
+        }
         break;
 
       // PUSH methods
       case "POST":
+        if (!payload.capabilities.includes("push")) {
+          console.warn(
+            `verifyToken: failed jwt verification: missing "push" capability for ${request.method} HTTP method`,
+          );
+          return { verified: false, payload: null };
+        }
+        if (!checkHasPermissionToImage(payload, request)) {
+          console.warn(
+            `verifyToken: failed jwt verification: image name ${payload?.imageName} does not match the token's image name in ${request.url}`,
+          );
+          return { verified: false, payload: null };
+        }
       case "PUT":
       case "DELETE":
       case "PATCH":
@@ -162,7 +189,6 @@ export class RegistryTokens implements Authenticator {
       default:
         return { verified: false, payload: null };
     }
-
     return { verified: true, payload };
   }
 
@@ -174,8 +200,21 @@ export class RegistryTokens implements Authenticator {
     if ("verified" in res) {
       return res;
     }
-
     const [, password] = res;
     return this.verifyToken(request, password);
   }
 }
+
+const checkHasPermissionToImage = (payload: RegistryAuthProtocolTokenPayload, request: Request) => {
+  const split = request.url.split("/");
+  let hasImageName = false;
+  for (const part of split) {
+    if (part === payload?.imageName) {
+      hasImageName = true;
+    }
+  }
+  if (!hasImageName) {
+    return false;
+  }
+  return true;
+};
